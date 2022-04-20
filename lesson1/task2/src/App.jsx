1746cbdcb921ab99d9360246a93219c2dc078b14
Vladyslav-Andrievych <vladandrievych@gmail.com>
2022-04-20 11:26:04 +0300

    added dist folder to .gitignore, react context 1 2

---
+++ lesson1/task2/src/App.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Header from './Header.jsx';
+import { UserContext } from './context.js';
+
+class App extends Component {
+  state = {
+    userData: {
+      name: 'Nikola Tesla',
+      avatar_url: 'https://avatars3.githubusercontent.com/u10001',
+    },
+  };
+
+  render() {
+    return (
+      <div className="page">
+        <UserContext.Provider value={this.state.userData}>
+          <Header />
+        </UserContext.Provider>
+      </div>
+    );
+  }
+}
+
+export default App;
